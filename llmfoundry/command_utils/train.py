cc485db992e4469837682fa8b8f7813906c8aafd
Brandon <bcui8377@gmail.com>
2025-01-28 04:44:58 +0000

    Add node and rank constraints

--- llmfoundry/command_utils/train.py
+++ llmfoundry/command_utils/train.py
@@ -247,16 +247,18 @@ def train(cfg: DictConfig) -> Trainer:
 
     print ("Before dist init")
 
-    vllm_engines = create_vllm_engines(
-        num_engines=num_vllm_engines,
-        tensor_parallel_size=tensor_parallel_size,
-        enforce_eager=True,
-        pretrain=vllm_model_name,
-        revision=None,
-        seed=1,
-        enable_prefix_caching=False,
-        max_model_len=4096,
-    )
+    if os.getenv('NODE_RANK', None) == '0' and os.getenv('LOCAL_RANK', None) == '0':
+
+        vllm_engines = create_vllm_engines(
+            num_engines=num_vllm_engines,
+            tensor_parallel_size=tensor_parallel_size,
+            enforce_eager=True,
+            pretrain=vllm_model_name,
+            revision=None,
+            seed=1,
+            enable_prefix_caching=False,
+            max_model_len=4096,
+        )
 
     print ("after vllm engines")
 
