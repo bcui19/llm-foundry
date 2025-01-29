c2a7c07ff4a481e49a3759e5fe376447ad96ca65
Brandon <bcui8377@gmail.com>
2025-01-29 02:35:22 +0000

    More debug

--- scripts/train/independent_ray_launcher.py
+++ scripts/train/independent_ray_launcher.py
@@ -14,8 +14,8 @@ from transformers import (
     AutoModelForCausalLM,
 )
 
-from vllm_utils import create_vllm_engines, init_process_group
-# from llmfoundry.utils.vllm_utils import create_vllm_engines, init_process_group
+# from vllm_utils import create_vllm_engines, init_process_group
+from llmfoundry.utils.vllm_utils import create_vllm_engines, init_process_group
 # from compose_rl.utils import print_debug_info
 # I think there's an import here that messes w/ ray...
 # import
