057b4ca0b1954dd3d4696eed1b8e9a93aafdd258
Brandon <bcui8377@gmail.com>
2025-01-29 01:47:05 +0000

    Make pass

--- llmfoundry/utils/vllm_utils.py
+++ llmfoundry/utils/vllm_utils.py
@@ -159,49 +159,50 @@ class WorkerWrap(Worker):
 class LLMRayActor:
 
     def __init__(self, *args, **kwargs):
-        import vllm
+        pass 
+        # import vllm
 
-        self.__version__ = vllm.__version__
-        assert self.__version__ >= '0.4.1', 'OpenRLHF only supports vLLM >= 0.4.1'
+        # self.__version__ = vllm.__version__
+        # assert self.__version__ >= '0.4.1', 'OpenRLHF only supports vLLM >= 0.4.1'
 
-        self.use_gpu_executor = kwargs['tensor_parallel_size'] == 1
-        print ("cuda visible devices in actor is: ", os.getenv('CUDA_VISIBLE_DEVICES', None))
-        print('kwargs are: ', kwargs)
+        # self.use_gpu_executor = kwargs['tensor_parallel_size'] == 1
+        # print ("cuda visible devices in actor is: ", os.getenv('CUDA_VISIBLE_DEVICES', None))
+        # print('kwargs are: ', kwargs)
 
-        # See https://github.com/vllm-project/vllm/blob/main/vllm/executor/gpu_executor.py
-        if self.use_gpu_executor:
+        # # See https://github.com/vllm-project/vllm/blob/main/vllm/executor/gpu_executor.py
+        # if self.use_gpu_executor:
 
-            vllm.worker.worker.Worker = WorkerWrap
-        else:
+        #     vllm.worker.worker.Worker = WorkerWrap
+        # else:
 
-            # RayGPUExecutor
-            # See the patch https://github.com/vllm-project/vllm/commit/479d69fad0538f04cb22bf13e76ff91cfeb8a4e5
-            # kwargs['worker_use_ray'] = True
+        #     # RayGPUExecutor
+        #     # See the patch https://github.com/vllm-project/vllm/commit/479d69fad0538f04cb22bf13e76ff91cfeb8a4e5
+        #     # kwargs['worker_use_ray'] = True
 
-            if vllm.__version__ > '0.4.1':
-                RayWorkerWrapperPath = vllm.executor.ray_utils
-            else:
-                RayWorkerWrapperPath = vllm.engine.ray_utils
+        #     if vllm.__version__ > '0.4.1':
+        #         RayWorkerWrapperPath = vllm.executor.ray_utils
+        #     else:
+        #         RayWorkerWrapperPath = vllm.engine.ray_utils
 
-            if vllm.__version__ > '0.6.4.post1':
-                # https://github.com/vllm-project/vllm/pull/10555
-                kwargs['worker_cls'] = 'vllm_utils.WorkerWrap'
-            else:
-                RayWorkerWrapperPath = vllm.engine.ray_utils
+        #     if vllm.__version__ > '0.6.4.post1':
+        #         # https://github.com/vllm-project/vllm/pull/10555
+        #         kwargs['worker_cls'] = 'vllm_utils.WorkerWrap'
+        #     else:
+        #         RayWorkerWrapperPath = vllm.engine.ray_utils
 
-                class RayWorkerWrapper(RayWorkerWrapperPath.RayWorkerWrapper):
+        #         class RayWorkerWrapper(RayWorkerWrapperPath.RayWorkerWrapper):
 
-                    def __init__(self, *args, **kwargs) -> None:
-                        # kwargs["worker_module_name"] = "open_instruct.vllm_utils2"
-                        kwargs['worker_module_name'] = 'vllm_utils'
-                        kwargs['worker_class_name'] = 'vllm_utils.WorkerWrap'
-                        super().__init__(*args, **kwargs)
+        #             def __init__(self, *args, **kwargs) -> None:
+        #                 # kwargs["worker_module_name"] = "open_instruct.vllm_utils2"
+        #                 kwargs['worker_module_name'] = 'vllm_utils'
+        #                 kwargs['worker_class_name'] = 'vllm_utils.WorkerWrap'
+        #                 super().__init__(*args, **kwargs)
 
-                RayWorkerWrapperPath.RayWorkerWrapper = RayWorkerWrapper
+        #         RayWorkerWrapperPath.RayWorkerWrapper = RayWorkerWrapper
 
-        self.llm = vllm.LLM(*args, **kwargs)
-        print('after vllm init')
-        print('kwargs are: ', kwargs)
+        # self.llm = vllm.LLM(*args, **kwargs)
+        # print('after vllm init')
+        # print('kwargs are: ', kwargs)
 
     def generate(self, *args, **kwargs):
         return self.llm.generate(*args, **kwargs)
